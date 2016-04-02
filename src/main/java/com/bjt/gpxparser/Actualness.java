c01bd2b16d380f0b72a1f87e3b5b4151c23c0e40
Ben Taylor <ben.taylor@ssp-uk.com>
2016-04-02 16:02:13 +0100

    correct error message

--- src/main/java/com/bjt/gpxparser/Actualness.java
+++ src/main/java/com/bjt/gpxparser/Actualness.java
@@ -11,10 +11,10 @@ public class Actualness {
             for(final TrackSegment trackSeg : track.getTrackSegments()) {
                 for(final TrackPoint  trackPoint : trackSeg.getTrackPoints()) {
                     if(trackPoint.getElevation() == null) {
-                        return new StatusResult(false, "At least one trackpoint has no elevation information");
+                        return new StatusResult(false, "At least one trackpoint has no elevation information.");
                     }
                     if(trackPoint.getTime() == null || trackPoint.getTime().isEmpty()) {
-                        return new StatusResult(false, "At least one trackpoint has no time information");
+                        return new StatusResult(false, "At least one trackpoint has no time information.");
                     }
                 }
             }
